2564887aa8f22fd290e8f37feefc3967bf26bffc
Jerang2 <wpckd324@gmail.com>
2025-08-17 20:20:58 +0900

    feat: sprintmission3

---
+++ routes/upload.router.js
@@ -0,0 +1,39 @@
+//upload route
+const express = requires('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { DESTRUCTION } = require('dns');
+
+//uploads 디렉토리가 없을 때 생성
+const uploadDir = 'uploads/';
+if (!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir);
+}
+
+//multer repository
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, Date.now() + ext);
+    },
+});
+
+//multer middleware
+const upload = multer({ storage: storage });
+
+//image api
+router.post('/upload', upload.single('image'), (req, res, next) => {
+    if (!req.file) {
+        return res.status(400).json({ message: '이미지 파일이 필요합니다.'});
+    }
+
+    const imageUrl = `/uploads/${req.file.filename}`;
+    res.status(201).json({ imageUrl: imageUrl });
+});
+
+module.exports = router;
