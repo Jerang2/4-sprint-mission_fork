d9b1629a0de65637b66ffdb0fee7bbf4d560fc5c
Jerang2 <wpckd324@gmail.com>
2025-09-15 01:29:27 +0900

    index.js에 cookie 미들웨어 추가

--- prisma/index.js
+++ prisma/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const cookieParser = require('cookie=parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ const uploadRouter = require('./routes/upload.router.js');
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'
 )));
