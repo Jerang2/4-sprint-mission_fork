ae377b5c56729fb8e951e14594562a47cf656d7f
Jerang2 <wpckd324@gmail.com>
2025-09-14 14:26:09 +0900

    UserService.js 생성

---
+++ UserService.js
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+export class UserService {    
+    prisma = new PrismaClient();
+
+    // 회원가입 로직
+    signUp = async (email, nickname, password) => {
+        
+        // 이메일 중복 확인
+        const existingUser = await this.prisma.user.findUnique({ where: { email } });
+        if (existingUser) {
+            throw new Error('이미 사용중인 이메일입니다.');
+        }
+
+        // 비밀번호 해싱
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // 유저 생성
+        const user = await this.prisma.user.create({
+            data: {
+                email,
+                nickname,
+                password: hashedPassword,
+            },
+        });
+
+        // 사용자 정보 반환
+        const { password: _, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    };
+}
