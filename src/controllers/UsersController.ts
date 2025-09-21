e4603531812dad14a894f57cfb61bdc26440ad3a
Jerang2 <wpckd324@gmail.com>
2025-09-21 16:15:38 +0900

    signUp 메서드 수정

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -20,7 +20,7 @@ class UsersController {
                 return res.status(400).json({ message: '모든 정보를 입력해주세요'});
             }
 
-            const newUser = await this.userService.signUp(email, nickname, password);
+            const newUser = await this.userService.signUp({ email, nickname, password });
 
             return res.status(201).json({
                 message: '회원가입이 완료되었습니다.',
